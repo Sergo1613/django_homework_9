cf31bacbfee90df326924ec23fb4fd8e3238332c
Sergo1613 <mentoss2012@gmail.com>
2023-08-29 00:20:09 +0300

    dj_homework_4

--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,33 @@
 from django.shortcuts import render
 
+from catalog.models import Category, Product
+
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'main/home.html')
+    context = {
+        'object_list': Category.objects.all()[:2],
+        'title': 'Главная - смартфоны'
+    }
+    return render(request, 'main/home.html', context)
+
+
+def categories(request):
+    context = {
+        'object_list': Category.objects.all(),
+        'title': 'Смартфоны - все модели'
+    }
+    return render(request, 'main/categories.html', context)
+
+
+def category_smartphone(request, pk):
+    category_item = Category.objects.get(pk=pk)
+    context = {
+        'object_list': Product.objects.filter(category_id=pk),
+        'title': f"Смартфоны - модели {category_item.name}"
+    }
+    return render(request, 'main/products.html', context)
 
 
 def contacts(request):
