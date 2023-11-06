695ce806b288a3d5b1a4b4ada1f3b8c4b730ca15
Sergo1613 <mentoss2012@gmail.com>
2023-11-06 22:00:49 +0300

    dj_hw_9

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from catalog.apps import CatalogConfig
 from catalog.views import ProductListView, ProductCreateView, ContactsView, ProductDetailView, ProductUpdateView, ProductDeleteView
@@ -10,7 +11,7 @@ urlpatterns = [
     path('', ProductListView.as_view(), name='index'),
     path('create/', ProductCreateView.as_view(), name='create_product'),
     path('contacts/', ContactsView.as_view(), name='contacts'),
-    path('view/<int:pk>/', ProductDetailView.as_view(), name='view_product'),
+    path('view/<int:pk>/', cache_page(60)(ProductDetailView.as_view()), name='view_product'),
     path('edit/<int:pk>/', ProductUpdateView.as_view(), name='edit_product'),
     path('delete/<int:pk>/', ProductDeleteView.as_view(), name='delete_product'),
 
--- config/settings.py
+++ config/settings.py
@@ -11,9 +11,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os.path
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
@@ -80,9 +83,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'home',
-        'USER': 'postgres',
-        'PASSWORD': 1613,
+        'NAME': os.getenv('NAME'),
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('PASSWORD'),
     }
 }
 
@@ -142,6 +145,15 @@ LOGIN_URL = '/users/'
 
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
-EMAIL_HOST_USER = 'fuckup@oscarbot.ru'
-EMAIL_HOST_PASSWORD = 'AsTSNVv7pun9'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_SSL = True
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": os.getenv('CACHE_LOCATION'),
+    }
+}
+
+
